528d0f2e8e420041544500d7407b7180a3f88157
Catalin <cioba.catalin@gmail.com>
2022-06-11 15:55:56 +0300

    Dockerize everything.

--- Backend/src/GoalController.js
+++ Backend/src/GoalController.js
@@ -1,7 +1,7 @@
 const dbHelper = require("./dbUtils");
 
 async function insertGoal(req) {
-    let sequelize = dbHelper.connectToDB();
+    let sequelize = await dbHelper.connectToDB();
     const [results] = await sequelize.query("INSERT INTO Goals (target_calories,proteins,fats,carbs) VALUES ("+
         req.body.target_calories + "," + req.body.proteins + "," + req.body.fats + "," + req.body.carbs + ")");
     sequelize.close();
@@ -10,7 +10,7 @@ async function insertGoal(req) {
 }
 
 async function getGoal(id){
-    let sequelize = dbHelper.connectToDB();
+    let sequelize = await dbHelper.connectToDB();
     const [result] = await sequelize.query("SELECT * FROM Goals WHERE id=" + id);
     sequelize.close();
 
