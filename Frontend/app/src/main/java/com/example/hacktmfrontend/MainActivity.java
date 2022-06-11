2f37ccff2a23d274bcc3333e1c547fec8cc4e37f
Silviu <batranutsilviu@yahoo.com>
2022-06-11 14:48:41 +0300

    merge comunicarea back-front

--- Frontend/app/src/main/java/com/example/hacktmfrontend/MainActivity.java
+++ Frontend/app/src/main/java/com/example/hacktmfrontend/MainActivity.java
@@ -12,6 +12,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hacktmfrontend.databinding.ActivityMainBinding;
 
 import android.view.Menu;
@@ -19,48 +25,10 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.IOException;
-import java.util.Arrays;
-
-import okhttp3.ConnectionSpec;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-
-//    final OkHttpClient client = new OkHttpClient();
-    OkHttpClient client = new OkHttpClient.Builder()
-            .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
-            .build();
-
-//AndroidNetworking.initialize(getApplicationContext(),okHttpClient)
-    String get(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
-
-    String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(JSON, json);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +50,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -115,13 +82,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText carbs = (EditText)findViewById(R.id.carbs);
         EditText fats = (EditText)findViewById(R.id.fats);
 
-        try {
-            this.post("http://10.10.9.209:3000/addFood","{'name':'gigea'}");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.10.9.209:3000/getGoal/1";
 
-//        Log.v("EditText", calories.getText().toString());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> Log.i("da", "Response is: " + response), new Response.ErrorListener() {
+            public void onErrorResponse(VolleyError error) {
+                Log.i("err", "Response is: " + error);
 
+            }
+        });
+        queue.add(stringRequest);
     }
 }
